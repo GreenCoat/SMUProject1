bfef55a9360acf994285afd09a22dacc828045cf
GreenCoat <nickscunningham@gmail.com>
2018-01-02 04:13:24 -0600

    Chat working at a basic level

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,13 +1,46 @@
 //Initialize Database
 var config = {
-    apiKey: "AIzaSyC-gZgN0pVkZrikTj2t8-coL7l8HtwqxsU",
-    authDomain: "classproject-d6e68.firebaseapp.com",
-    databaseURL: "https://classproject-d6e68.firebaseio.com",
-    projectId: "classproject-d6e68",
-    storageBucket: "classproject-d6e68.appspot.com",
-    messagingSenderId: "769292531793"
+    apiKey: "AIzaSyBSr-0kuZwXxHJgFYWeQIf3pn2alAAD3cQ",
+    authDomain: "myfirstproject-b539b.firebaseapp.com",
+    databaseURL: "https://myfirstproject-b539b.firebaseio.com",
+    projectId: "myfirstproject-b539b",
+    storageBucket: "myfirstproject-b539b.appspot.com",
+    messagingSenderId: "51096609060"
   };
 
 firebase.initializeApp(config);
 
 var database = firebase.database();
+
+//Wait for document to load
+$(document).ready(function(){
+
+	//On click button for saving chat messages to DB
+	$("#send").on("click", function(){
+		//Keep submit button from refreshing the page
+		event.preventDefault();
+
+		//Get message from message field
+		var message = $("#chat-message").val().trim();
+
+		//Validation, currently checking if message is empty
+		if(message != ""){
+			database.ref("/messages").push({
+				message: message
+			});
+		}
+	
+		//Clear out message field
+		$("#chat-message").val("");
+	});
+
+
+	//Update event for DB that also retrieves messages
+	database.ref("/messages").on("child_added", function(snapshot){
+		//Retrieves data snapshot
+		var sv = snapshot.val();
+
+		//Puts chat message in chat window
+		$("#chat-window").append("<div>"+sv.message+"</div>");
+	});
+});
